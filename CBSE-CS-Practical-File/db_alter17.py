7c8b856bf9ec6a3860f353f24811cca5f69272aa
Utkarsh Luthra <utkarshluthra@gmail.com>
2020-12-10 14:27:14 +0530

    Delete db_alter17.py

--- CBSE-CS-Practical-File/db_alter17.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="root",database="school")
-mycursor=mydb.cursor()
-mycursor.execute("alter table student add (city varchar(10))")
