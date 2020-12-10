aff65f77a95e21a0d247ef1bf6b0e3b9d49b3533
Utkarsh Luthra <utkarshluthra@gmail.com>
2020-12-10 14:28:25 +0530

    Delete db_select19.py

--- CBSE-CS-Practical-File/db_select19.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="root",database="school")
-mycursor=mydb.cursor()
-mycursor.execute("select * from student where age>12")
-myres=mycursor.fetchall()
-for x in myres:
-    print(x)
