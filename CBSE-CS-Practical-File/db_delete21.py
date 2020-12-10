5acc5848d13186fc4f4021bab781140366831aa2
Utkarsh Luthra <utkarshluthra@gmail.com>
2020-12-10 14:27:28 +0530

    Delete db_delete21.py

--- CBSE-CS-Practical-File/db_delete21.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="root",database="school")
-mycursor=mydb.cursor()
-sql="delete from student where rollno=102"
-mycursor.execute(sql)
-mydb.commit()
-print(mycursor.rowcount,"record deleted")
